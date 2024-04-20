c86171b8745ba8bcbbed0bf1a970cf815feb0b1d
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2024-04-20 09:31:06 +0200

    drivers: himax: 83102E: Add virtual touchpad (support_dex)
    From N770FXXU9HWI2

--- drivers/input/touchscreen/himax/himax_83102E/himax_platform_SPI.c
+++ drivers/input/touchscreen/himax/himax_83102E/himax_platform_SPI.c
@@ -50,11 +50,19 @@ int himax_dev_set(struct himax_ts_data *ts)
 
 	ts->hx_pen_dev->name = "himax-pen";
 #endif
+
+	if (ts->pdata->support_dex) {
+		ts->input_dev_pad = input_allocate_device();
+		if (ts->input_dev_pad == NULL) {
+			ret = -ENOMEM;
+			E("%s: Failed to allocate input device-hx_dex_dev\n", __func__);
+			return ret;
+		}
+
+		ts->input_dev_pad->name = "sec_touchpad";
+	}
+
 	return ret;
-}
-int himax_input_register_device(struct input_dev *input_dev)
-{
-	return input_register_device(input_dev);
 }
 
 #if defined(HX_PLATFOME_DEFINE_KEY)
@@ -200,6 +208,7 @@ int himax_parse_dt(struct himax_ts_data *ts,
 	}
 
 	pdata->support_aot = of_property_read_bool(dt, "support_aot");
+	pdata->support_dex = of_property_read_bool(dt, "support_dex_mode");
 
 	himax_vk_parser(dt, pdata);
 	return 0;
